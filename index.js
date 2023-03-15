b9a4f3b6df138e53df233e5ef76294862cf78084
Krishna <itshree001@gmail.com>
2023-03-15 11:12:41 +1100

    Done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 =31;
+let num2=2;
+let multiply;
+multiply= num1* num2;
+
+let random= Math.round(Math.random()*100)
+
+let num4=5;
+let num3=24;
+let mod=num3 % num4;
+
+let max= Math.max(1,3,5,6,7,8,20);
